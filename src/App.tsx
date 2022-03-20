597f88d5d244fb3beb530138dac3954422406335
Matsushoooo12 <matsush.oooo12@icloud.com>
2022-03-20 12:24:46 +0900

    Chakra-UI導入＆新規アンケート作成機能

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
 import React from "react";
 import { HashRouter, Switch, Route } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
 import { Home } from "./components/Home";
 import { Page404 } from "./components/Page404";
 function App() {
   return (
-    <HashRouter>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="*">
-          <Page404 />
-        </Route>
-      </Switch>
-    </HashRouter>
+    <ChakraProvider>
+      <HashRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="*">
+            <Page404 />
+          </Route>
+        </Switch>
+      </HashRouter>
+    </ChakraProvider>
   );
 }
 
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,44 @@
+import {
+  Box,
+  Button,
+  Heading,
+  Input,
+  InputGroup,
+  Text,
+} from "@chakra-ui/react";
 import { useEffect, useState, VFC } from "react";
-import { getAllPosts } from "../api/post";
+import { createPost, getAllPosts } from "../api/post";
 import { Post } from "../types/post";
 
 export const Home: VFC = () => {
   const [posts, setPosts] = useState<Post[]>([]);
+
+  const [value, setValue] = useState({
+    id: 0,
+    title: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue({
+      ...value,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    try {
+      const res = await createPost(value);
+      console.log(res.data);
+      handleGetAllPosts();
+    } catch (e) {
+      console.log(e);
+    }
+    setValue({
+      id: 0,
+      title: "",
+    });
+  };
 
   const handleGetAllPosts = async () => {
     try {
@@ -19,12 +54,45 @@ export const Home: VFC = () => {
     handleGetAllPosts();
   }, []);
   return (
-    <div>
+    <Box mt="24px" ml="24px">
+      <Box mb="40px">
+        <Heading mb="16px">新規アンケート作成</Heading>
+        <form>
+          <InputGroup w="500px">
+            <Input
+              placeholder="title"
+              type="text"
+              variant="outline"
+              mr="16px"
+              onChange={handleChange}
+              value={value.title}
+              name="title"
+            />
+            <Button
+              bg="teal"
+              color="white"
+              type="submit"
+              onClick={(e) => handleSubmit(e)}
+            >
+              作成
+            </Button>
+          </InputGroup>
+        </form>
+      </Box>
       {posts.map((post) => (
-        <div key={post.id}>
-          <p>{post.title}</p>
-        </div>
+        <Box
+          border="1px solid black"
+          display="inline-block"
+          width="500px"
+          p="24px"
+          borderRadius="md"
+          key={post.id}
+          mb="24px"
+          cursor="pointer"
+        >
+          <Text>{post.title}</Text>
+        </Box>
       ))}
-    </div>
+    </Box>
   );
 };
